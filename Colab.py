f043716acf7a851e48c7f22d3430ceaecd7dabd9
Vittorio Zoccola <133058715+vittorio-zoccola@users.noreply.github.com>
2024-08-12 17:53:05 +0100

    Update Colab.py

--- Colab.py
+++ Colab.py
@@ -217,30 +217,14 @@ for filename in os.listdir(folder_path):
 
 
 
-
-
-
-
-!pip install roboflow
-
-from roboflow import Roboflow # remove extra indentation
-rf = Roboflow(api_key="//")
-project = rf.workspace("//").project("//")
-version = project.version(//)
-dataset = version.download("//")
-
-
-!nvidia-smi
-
+✅ #YOLOv10n VERSION N.1 
 
 import os
 HOME = os.getcwd()  # Use os.getcwd() instead of os.getcw()
 print(HOME)
 
 !pip install -q supervision
-
 !pip install -q git+https://github.com/THU-MIG/yolov10.git
-
 
 !mkdir -p {HOME}/weights
 !wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10n.pt
@@ -257,4 +241,79 @@ model={HOME}/weights/yolov10n.pt \
 data={dataset.location}/data.yaml
 
 while True:pass
+
+
+
+
+✅ #YOLOv10n + Custom Learnign Rate (VERSION N.2) 
+
+import os
+HOME = os.getcwd()  # Use os.getcwd() instead of os.getcw()
+print(HOME)
+
+!pip install -q supervision
+!pip install -q git+https://github.com/THU-MIG/yolov10.git
+
+!mkdir -p {HOME}/weights
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10n.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10s.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10m.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10b.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10x.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10l.pt
+!ls -lh {HOME}/weights
+
+%cd {HOME}
+!yolo task=detect mode=train epochs=25 batch=8 plots=TRUE \
+model={HOME}/weights/yolov10n.pt \
+data={dataset.location}/data.yaml \
+lr0=0.01  # Custom Learning Rate
+
+while True:pass
+
+
+
+
+✅ #YOLOv10n + Adjusting Batch Size and Learning Rate (VERSION N.3) 
+
+import os
+HOME = os.getcwd()  # Use os.getcwd() instead of os.getcw()
+print(HOME)
+
+!pip install -q supervision
+!pip install -q git+https://github.com/THU-MIG/yolov10.git
+
+!mkdir -p {HOME}/weights
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10n.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10s.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10m.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10b.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10x.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10l.pt
+!ls -lh {HOME}/weights
+
+%cd {HOME}
+!yolo task=detect mode=train epochs=50 batch=16 plots=TRUE \
+model={HOME}/weights/yolov10n.pt \
+data={dataset.location}/data.yaml \
+lr0=0.01  # Custom Learning Rate
+
+while True:pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
