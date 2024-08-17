a611a4bf32cf92740b280c54cfc72eba3c9877f2
Vittorio Zoccola <133058715+vittorio-zoccola@users.noreply.github.com>
2024-08-17 18:10:25 +0100

    Update Colab.py

--- Colab.py
+++ Colab.py
@@ -237,7 +237,7 @@ print(HOME)
 !ls -lh {HOME}/weights
 
 %cd {HOME}
-!yolo task=detect mode=train epochs=25 batch=8 plots=TRUE \
+!yolo task=detect mode=train epochs=12 batch=4 plots=TRUE \
 model={HOME}/weights/yolov10n.pt \
 data={dataset.location}/data.yaml
 
@@ -268,13 +268,35 @@ print(HOME)
 !yolo task=detect mode=train epochs=25 batch=8 plots=TRUE \
 model={HOME}/weights/yolov10n.pt \
 data={dataset.location}/data.yaml \
-lr0=0.007  # Custom Learning Rate
 
 while True:pass
     
 =====================================================================================================================
 
+✅ #YOLOv10n VERSION N.3 
 
+
+import os
+HOME = os.getcwd()  # Use os.getcwd() instead of os.getcw()
+print(HOME)
+
+!pip install -q supervision
+!pip install -q git+https://github.com/THU-MIG/yolov10.git
+
+!mkdir -p {HOME}/weights
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10n.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10s.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10m.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10b.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10x.pt
+!wget -P {HOME}/weights -q https://github.com/jameslahm/yolov10/releases/download/v1.0/yolov10l.pt
+!ls -lh {HOME}/weights
+
+%cd {HOME}
+!yolo task=detect mode=train epochs=25 batch=8 plots=TRUE \
+model={HOME}/weights/yolov10n.pt \
+data={dataset.location}/data.yaml \
+lr0=0.007  # Custom Learning Rate
 
 
 
